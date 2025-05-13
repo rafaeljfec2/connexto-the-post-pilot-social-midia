c6450ec170053474e0e7aab8e7bfcacd28b98c72
rafaeljfec2 <rafael.ferreira2@thomsonreuters.com>
2025-05-12 23:40:47 -0300

    destak card

--- apps/web/src/components/ui/card.tsx
+++ apps/web/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-white dark:bg-zinc-900 border-zinc-200 dark:border-zinc-700 shadow-lg",
       className
     )}
     {...props}
