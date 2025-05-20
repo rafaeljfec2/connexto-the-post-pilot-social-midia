6860c7f97af1d4eed76abd8af7ff2d0ac1181b3e
rafaeljfec2 <rafael.ferreira2@thomsonreuters.com>
2025-05-20 17:01:13 -0300

    vscode

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,36 +1,39 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
-      "@components": resolve(__dirname, "./src/components"),
-      "@lib": resolve(__dirname, "./src/lib"),
-      "@hooks": resolve(__dirname, "./src/hooks"),
-      "@store": resolve(__dirname, "./src/store"),
-      "@styles": resolve(__dirname, "./src/styles"),
-      "@types": resolve(__dirname, "./src/types"),
-      "@utils": resolve(__dirname, "./src/utils"),
-      "@services": resolve(__dirname, "./src/services"),
-      "@assets": resolve(__dirname, "./src/assets"),
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@lib': resolve(__dirname, './src/lib'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@store': resolve(__dirname, './src/store'),
+      '@styles': resolve(__dirname, './src/styles'),
+      '@types': resolve(__dirname, './src/types'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@services': resolve(__dirname, './src/services'),
+      '@assets': resolve(__dirname, './src/assets'),
     },
   },
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      '/the-post-pilot/v1': {
+        target: 'http://localhost:8081',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-});
+  build: {
+    sourcemap: true,
+  },
+})
